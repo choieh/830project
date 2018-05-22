d7e554b3b27ff17a23cac2c84443d552c272f1aa
hyunjo7o5 <nhj19111@gmail.com>
2018-05-22 18:30:28 +0900

    ㅎㅈ
    ㅎㅈ

--- js/script.js
+++ js/script.js
@@ -58,6 +58,7 @@ $(function(){
 	$(".loading").delay(350).fadeOut(1000);
 	
 	$("#home").height($(window).height());//DOM트리 구성이 완료된 시점(ready)에 높이값 설정
+	$("#about").height($(window).height());//DOM트리 구성이 완료된 시점(ready)에 높이값 설정
 	$("#services").height($(window).height());
 	$("#portfolio").height($(window).height());
 	$("#contact").height($(window).height());
